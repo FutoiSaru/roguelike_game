202901c2cbcba7b18e5a9bfd1e908a5f4a4b5b62
FutoiSaru <futoisaru@gmail.com>
2017-08-29 22:23:51 +0100

    Added ability to move

--- game.py
+++ game.py
@@ -6,11 +6,30 @@ import libtcodpy as libtcod
 import constants
 
 
+# STRUCTS
 class struc_Tile:
 	def __init__(self, block_path):
 		self.block_path = block_path
 
 
+# OBJECTS
+class obj_Actor:
+	def __init__(self, x, y, sprite):
+		# Map address
+		self.x = x
+		self.y = y
+		self.sprite = sprite
+
+	def draw(self):
+		SURFACE_MAIN.blit(self.sprite, (self.x * constants.CELL_WIDTH, self.y * constants.CELL_HEIGHT))
+
+	def move(self, dx, dy):
+		if GAME_MAP[self.x + dx][self.y + dy].block_path is False:
+			self.x += dx
+			self.y += dy
+
+
+# MAP
 # Create map
 def map_create():
 	new_map = [[struc_Tile(False) for y in range(0, constants.MAP_HEIGHT)] for x in range(0, constants.MAP_WIDTH)]
@@ -21,6 +40,7 @@ def map_create():
 	return new_map
 
 
+# DRAWING
 # Function to draw game
 def draw_game():
 	global SURFACE_MAIN
@@ -31,7 +51,7 @@ def draw_game():
 	draw_map(GAME_MAP)
 
 	# Draw character
-	SURFACE_MAIN.blit(constants.SPR_PLAYER, (200, 200))
+	PLAYER.draw()
 
 	# Update display
 	pygame.display.flip()
@@ -41,13 +61,14 @@ def draw_map(map_to_draw):
 	for x in range(0, constants.MAP_WIDTH):
 		for y in range(0, constants.MAP_HEIGHT):
 			# Draw wall
-			if map_to_draw[x][y].block_path == True:
+			if map_to_draw[x][y].block_path is True:
 				SURFACE_MAIN.blit(constants.SPR_WALL, (x * constants.CELL_WIDTH, y * constants.CELL_HEIGHT))
 			# Draw floor
 			else:
 				SURFACE_MAIN.blit(constants.SPR_FLOOR, (x * constants.CELL_WIDTH, y * constants.CELL_HEIGHT))
 
 
+# GAME
 # Function to loop main game
 def game_main_loop():
 	game_quit = False
@@ -63,6 +84,16 @@ def game_main_loop():
 			if event.type == pygame.QUIT:
 				game_quit = True
 
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_UP:
+					PLAYER.move(0, -1)
+				if event.key == pygame.K_DOWN:
+					PLAYER.move(0, 1)
+				if event.key == pygame.K_LEFT:
+					PLAYER.move(-1, 0)
+				if event.key == pygame.K_RIGHT:
+					PLAYER.move(1, 0)
+
 		# Draw game
 		draw_game()
 
@@ -74,7 +105,7 @@ def game_main_loop():
 # Function to initialize the game
 def game_initialize():
 
-	global SURFACE_MAIN, GAME_MAP
+	global SURFACE_MAIN, GAME_MAP, PLAYER
 
 	# Initialize pygame
 	pygame.init()
@@ -83,6 +114,8 @@ def game_initialize():
 		(constants.GAME_WIDTH, constants.GAME_HEIGHT))
 
 	GAME_MAP = map_create()
+
+	PLAYER = obj_Actor(0, 0, constants.SPR_PLAYER)
 
 
 # Execute Game
